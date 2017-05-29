12ad7039c17df2c89f6218080c752239b29906d9
ailing <zoezoe129@hotmail.com>
2017-05-29 14:16:33 +0800

    finish basic lab1

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,29 @@
 import argparse
 import json
 import os
+import re
+
 
 def execute_cmd(cmd):
     print "***** Executing command '"+ cmd + "'"
     response = os.popen(cmd).read()
     return response
+  
+def process_commits():
+    cmd = "cd " + project + "; git log --all --since=" + str(no_days) + ".day --name-status"
+    response = execute_cmd(cmd)
+    for line in response.splitlines():
+        if line.startswith('commit'):       
+            print line[7:]
+        if line.startswith('Author:'):
+            if(re.search('\<(.*?)\>',line)):
+                print re.search('\<(.*?)\>',line).group(1)
+        if line.startswith('Date:'):
+            print line[5:]
 
 parser = argparse.ArgumentParser(description="Code Review Scheduler Program")
 parser.add_argument("-n", nargs="?", type=int, default=1, help="Number of (d)ays to look for log. ")
-parser.add_argument("-p", nargs="?", type=str, default="project_x", help="Project name.")
+parser.add_argument("-p", nargs="?", type=str, default="project_ex", help="Project name.")
 args = parser.parse_args()
  
 no_days = args.n
@@ -29,7 +43,7 @@ for p in main_config:
         project_url = p['git_url']
     break
    
-print(project_url)
+
 # Clone the repository if not already exists
 print "********* Doing project checkout **********"
 if(os.path.isdir("./" + project)):
@@ -38,3 +52,7 @@ else:
     execute_cmd("git clone " + project_url + " " + project)
 print "*** Done *******"
 print " "
+
+
+print 'Processing the scheduler against project ' + project + '....'
+process_commits()
